0c5ef40f80e739294ef409994c142927754a3569
joel gs <joelpaginasweb@gmail.com>
2024-06-17 05:40:53 -0500

    ajuste 17 jun

--- resources/js/ventana_popup.js
+++
@@ -1,9 +0,0 @@
-
-
-function abrir (){
-    document.getElementById("emergente").style.display="block";
-} /*- al llamar la funcion abrir(), entra al documento, se conecta con "vent" y le aplica un display block y se hace visible -*/
-
-function cerrar (){
-    document.getElementById("emergente").style.display="none";
-} /*- al llamar la funcion cerrar(), entra al documento, se conecta con "vent" y le aplica un display none y se oculta  -*/
