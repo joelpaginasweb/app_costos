c228dcaf492967bf2d6f13802facff3193d75603
joelpaginasweb <joelpaginasweb@gmail.com>
2024-09-26 13:13:08 -0500

    se agrego funcion open user

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -12,19 +12,18 @@ import './functions/open_user';
 import './functions/select_one_click';
 
 // ------------------------------
-
-// import './functions/crear_fila_material';
-// import './functions/crear_fila_MO';
-// import './functions/crear_fila_equipo';
+import './functions/crear_fila_material';
+import './functions/crear_fila_MO';
+import './functions/crear_fila_equipo';
 
 // ------------------------------
 
 //tiene conflicto con la funcion desplegar user
 //se agrego <div id="boton_crear_aux"></div> en footer_app para evitar conflicto
 // import './functions/crea_element_aux';
-// import './functions/crea_element_aux_mod';
+import './functions/crea_element_aux_mod';
 
-//tiene conflicto con la funcion desplegar user
+
 import './functions/crea_element_table';
 
 
