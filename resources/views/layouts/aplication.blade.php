b55fad5c0fbb12c160956e66636eb07e0badb4cb
joelpaginasweb <joelpaginasweb@gmail.com>
2024-09-27 02:55:19 -0500

    intento de produccion

--- resources/views/layouts/aplication.blade.php
+++ resources/views/layouts/aplication.blade.php
@@ -7,8 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!----------------Enlaces css y js----------->
-  @vite(['resources/css/app.css'])
-  @vite(['resources/js/app.js'])
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  {{-- @vite(['resources/css/app.css']) --}}
+  {{-- @vite(['resources/js/app.js']) --}}
 
   <!--------------titulo documento------------>
   <title>App Costos - @yield('title')</title>
